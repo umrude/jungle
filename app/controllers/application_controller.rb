905fd47dbb278d3d544468cdd18b4bdf745623c5
Gabi Geollegue <gabigeollegue@gmail.com>
2019-11-12 23:55:09 +0000

    debug missing $ and missing decimal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,8 @@ class ApplicationController < ActionController::Base
   helper_method :cart_subtotal_cents
 
 
+
+
   def update_cart(new_cart)
     cookies[:cart] = {
       value: JSON.generate(new_cart),
