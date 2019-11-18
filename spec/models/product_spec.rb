1e7077038069c8767ca17c9885cc0bbfef358111
Gabi Geollegue <gabigeollegue@gmail.com>
2019-11-18 21:35:37 +0000

    rspec: add test for product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    let (:category){ Category.create(name: "testCat")}
+    subject { Product.new(name: "test", price_cents: 10000, quantity: 1000, category_id: category.id) } 
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a price" do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+
+    end
+
+    it "is not valid without a quantity" do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+
+    end
+
+    it "is not valid without a category" do 
+      subject.category_id = nil
+      expect(subject).to_not be_valid
+
+    end
+
+
+  end
+end
+
