e1ed3880bcaddff2ea90d6ec3a3b1369721468f8
Gabi Geollegue <gabigeollegue@gmail.com>
2019-11-19 01:54:51 +0000

    refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@
 class UsersController < ApplicationController
   def new; end
 
-<<<<<<< HEAD
     def create
         user = User.new(user_params)
         if user.save
@@ -13,27 +12,10 @@ class UsersController < ApplicationController
             redirect_to '/signup'
         end
     end 
-=======
-  def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
-    else
-      redirect_to '/signup', flash: { error: 'You entered something wrong here and Please make sure you enter all the fields correct' }
-    end
-  end
->>>>>>> efdf1b553b0c5d83adc6631b55a82318cb0c63ab
 
   private
 
   def user_params
-    params.require(:user).permit(
-      :first_name,
-      :last_name,
-      :email,
-      :password,
-      :password_confirmation
-    )
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,8 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     subject do
       User.new(
-<<<<<<< HEAD
         id: 1,
         name: 'first',
-=======
-        first_name: 'first',
-        last_name: 'last',
->>>>>>> efdf1b553b0c5d83adc6631b55a82318cb0c63ab
         email: 'test@test.com',
         password: 'password',
         password_confirmation: 'password'
@@ -26,48 +21,39 @@ RSpec.describe User, type: :model do
     it 'is not valid without a email' do
       subject.email = nil
       expect(subject).to_not be_valid
-      expect(subject.errors[:email]).to_not be_empty
     end
 
     it 'is not valid without a first name' do
-      subject.first_name = nil
+      subject.name = nil
       expect(subject).to_not be_valid
-      expect(subject.errors[:first_name]).to_not be_empty
-    end
-
-    it 'is not valid without a last name' do
-      subject.last_name = nil
-      expect(subject).to_not be_valid
-      expect(subject.errors[:last_name]).to_not be_empty
     end
 
     it 'is not valid with a password less than the minimum length' do
       subject.password = 'ggeo'
       expect(subject).to_not be_valid
-      expect(subject.errors[:password]).to_not be_empty
     end
   end
 
   describe '.authenticate_with_credentials' do
-    let!(:user) { described_class.create!(first_name: 'here', last_name: 'there', email: 'test@test.com', password: '12345678', password_confirmation: '12345678') }
+    let!(:user) { described_class.create!(name: "Gabi", email: 'test@test.com', password: 'password', password_confirmation: 'password') }
 
     it 'is valid with correct password' do
-      result = User.authenticate_with_credentials('test@test.com', '12345678')
+      result = User.authenticate_with_credentials('test@test.com', 'password')
       expect(result).to be_present
     end
 
     it 'is not valid with incorrect password' do
-      result = User.authenticate_with_credentials(' mi@test.com ', '12345678')
+      result = User.authenticate_with_credentials(' mi@test.com ', 'password')
       expect(result).to be_nil
     end
 
     it 'is valid with correct password with space' do
-      result = User.authenticate_with_credentials(' test@test.com ', '12345678')
+      result = User.authenticate_with_credentials(' test@test.com ', 'password')
       expect(result).to be_present
     end
 
     it 'is valid with correct password with uppercase' do
-      result = User.authenticate_with_credentials(' test@tEsT.com ', '12345678')
+      result = User.authenticate_with_credentials(' test@tEsT.com ', 'password')
       expect(result).to be_present
     end
   end
