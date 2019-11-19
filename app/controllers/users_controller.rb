d48283bd71a3d8d6670729f3c15e65eb810a93ba
Gabi Geollegue <gabigeollegue@gmail.com>
2019-11-19 01:45:16 +0000

    refactor user_spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,15 @@
 class UsersController < ApplicationController
   def new; end
 
-  def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
-    else
-      redirect_to '/signup'
-    end
-  end
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            redirect_to '/'
+        else
+            redirect_to '/signup'
+        end
+    end 
 
   private
 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    subject do
+      User.new(
+        id: 1,
+        name: 'first',
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+    end
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a first name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with a password less than the minimum length' do
+      subject.password = 'ggeo'
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    let!(:user) { described_class.create!(name: "Gabi", email: 'test@test.com', password: 'password', password_confirmation: 'password') }
+
+    it 'is valid with correct password' do
+      result = User.authenticate_with_credentials('test@test.com', 'password')
+      expect(result).to be_present
+    end
+
+    it 'is not valid with incorrect password' do
+      result = User.authenticate_with_credentials(' mi@test.com ', 'password')
+      expect(result).to be_nil
+    end
+
+    it 'is valid with correct password with space' do
+      result = User.authenticate_with_credentials(' test@test.com ', 'password')
+      expect(result).to be_present
+    end
+
+    it 'is valid with correct password with uppercase' do
+      result = User.authenticate_with_credentials(' test@tEsT.com ', 'password')
+      expect(result).to be_present
+    end
+  end
+end
